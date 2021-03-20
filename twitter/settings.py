36239d89428cd667459fc31fd6ce55ddd72c7691
Jalon Dodson <jalon.dodson@yahoo.com>
2021-03-20 17:00:46 -0500

    update settings.py

--- twitter/settings.py
+++ twitter/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# import environ
-# env = environ.Env(
-#     DEBUG=(bool, False)
-# )
+import environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
