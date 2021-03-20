e15f8dfa7170b15046faefba9b16b7c1c5e4164d
Jalon Dodson <jalon.dodson@yahoo.com>
2021-03-20 17:03:56 -0500

    fixed secret key

--- twitter/settings.py
+++ twitter/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # for heroku:
+environ.Env.read_env()
 SECRET_KEY = os.environ["secret_key"]
-# environ.Env.read_env()
 # SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
